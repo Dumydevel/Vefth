d40dd0d83f6e31ce22959bd5dcb47319f01062a1
Dumydevel <kajjo282@nemi.tskoli.is>
2018-01-15 11:45:41 +0000

    Create verkefni1.py

---
+++ verkefni1.py
@@ -0,0 +1,42 @@
+from bottle import *
+
+@route('/')
+def vid():
+    return """
+    <!DOCTYPE html>
+           <html>
+               <head>
+                   <title>Mass effect</title>
+                   <meta charset="utf-8">
+                   
+
+               </head>
+               <body>
+                   <ul>
+                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/vsh2/verkefni6">Mass effect</a></li>
+                       <li><a href="http://tsuts.tskoli.is/2t/0711992829/Verkefni1/verkefni1.html">Biography</a></li>
+                       <li><a href="/pic">Pictures</a></li>
+                   </ul>
+
+                   <h1>Mass effect</h1>
+                    <img src="static/mass-effect-3.jpg" alt="Mass effect">
+               </body>
+           </html>
+        
+
+
+    """
+@route('/verkefni6/')
+def verk6 ():
+    return static_file(filename="http://tsuts.tskoli.is/2t/0711992829/vsh2/verkefni6",root="http://tsuts.tskoli.is/2t/0711992829/vsh2/verkefni6")
+@route('/static/<skra>')
+def static_skrar(skra):
+    return static_file(skra, root='./public/')
+
+@error(404)
+def error404(error):
+    return '<p>UmbeÃƒÂ°in sÃƒÂ­ÃƒÂ°a er ekki til</p><a href="/">Mass effect</a>'
+
+run(host='localhost', port=8080, debug=True)
+
+
